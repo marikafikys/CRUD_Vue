d930251f34617fc4072258f86ca280de6a41c898
marikafikys <marikaafikys@gmail.com>
2023-07-27 21:49:27 +0300

    add validation

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 Vue.createApp({
   data() {
     return {
+      popupTitle: '',
+      errors: [],
       isOpen: false,
-      valueInput: '',
       lastName: '',
       firstName: '',
       email: '',
@@ -19,48 +20,71 @@ Vue.createApp({
     }
   },
   methods: {
-    // showPopup (isOpen = false) {
-    //   console.log('show', isOpen);
-    //   return isOpen;
-    // },
-    setShowPopup(value) {
+    setShowPopup(value, mode) {
       this.isOpen = value;
-      // console.log('set', isOpen);
-      // this.showPopup(isOpen);
+      if (mode === 'add') {
+        this.popupTitle = 'Create new user'
+      } else if (mode === 'edit') {
+        this.popupTitle = 'Edit user '+this.firstName+' '+this.lastName;
+      }
     },
-    // handleInput (event) {
-    //   this.valueInput = event.target.value;
-    // },
+    checkForm () {
+      this.errors = [];
+      if (!this.firstName) {
+        this.errors.push('Enter your first name');
+      }
+      if (!this.lastName) {
+        this.errors.push('Enter your last name');
+      }
+      if (!this.validEmail(this.email) && this.email.length !== 0) {
+        this.errors.push('Укажите корректный адрес электронной почты.');
+      }
+      if (!this.errors.length) {
+        return true;
+      }
+      // e.preventDefault();
+    },
+    validEmail (email) {
+      var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
+    },
     addUser () {
-      // console.log(event.target.value);
-      // if(this.valueInput === '') { return };
-      this.user = {
-        lastName: this.lastName,
-        firstName: this.firstName,
-        email: this.email,
-        site: this.site,
-        avatar: this.avatar,
-        birthday: this.birthday.split('-').reverse().join('.'),
-        id: Math.round(Math.random()*1000)
-      };
-      this.userList.push(this.user);
-      this.isOpen = false;
-      this.saveUsers();
+      if (this.checkForm()) {
+        this.user = {
+          lastName: this.lastName,
+          firstName: this.firstName,
+          email: this.email,
+          site: this.site,
+          avatar: this.avatar,
+          birthday: this.birthday.split('-').reverse().join('.'),
+          id: Math.round(Math.random()*1000)
+        };
+        this.userList.push(this.user);
+        this.isOpen = false;
+        this.saveUsers();
+      }
     },
     saveUsers() {
       const parsed = JSON.stringify(this.userList);
       localStorage.setItem('userList', parsed);
     },
-    // doCheck (index, type) {
-    //   if(type === 'need') {
-    //     const completeMask = this.needDoList.splice(index, 1);
-    //     this.completeList.push(...completeMask);
-    //   }
-    //   else {
-    //     const noCompleteMask = this.completeList.splice(index, 1);
-    //     this.needDoList.push(...noCompleteMask);
-    //   }
-    // },
+    editUser(index) {
+      let currentUser = this.userList[index];
+      this.user = currentUser;
+      this.user.lastName = currentUser.lastName;
+      console.log(currentUser.lastName);
+      console.log(this.user.lastName);
+      this.setShowPopup(true, 'edit');
+      // this.user = {
+      //   lastName: currentUser.lastName,
+      //   firstName: currentUser.firstName,
+      //   email: currentUser.email,
+      //   site: currentUser.site,
+      //   avatar: currentUser.avatar,
+      //   birthday: currentUser.birthday,
+      //   id: currentUser.id
+      // };
+    },
     removeUser (index) {
       if (confirm("Are you sure?")) {
         this.userList.splice(index, 1);
